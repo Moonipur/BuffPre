4fb69ad19701db43c5cabbacd4519ab4211a59d5
Moonipur <119776865+Moonipur@users.noreply.github.com>
2022-12-13 01:26:40 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ if [ -f ~/.bashrc ];
 then
    Loc=`find "$(cd ..; pwd)" -name "BuffPre.py"`;
    echo "alias BuffPre='python3 ${Loc}'" >> ~/.bashrc;
+   echo "Installation is already finished";
 else
    echo "Your .bashrc does NOT exist";
 fi
